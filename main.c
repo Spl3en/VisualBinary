4e96bca5999294b4dd287d96f0e885a4236cd3ea
Moreau Cyril <spl3en.contact@gmail.com>
2014-08-12 22:09:33 +0200

    Code cleaning

--- main.c
+++ main.c
@@ -6,42 +6,45 @@
 int main(int argc, char **argv)
 {
 	char *filename = NULL;
-	srand(time(NULL));
 
-	if (argc < 2)
-		filename = "verdana.ttf";
-	else
-    {
-        printf("Loading %s ...\n", argv[1]);
-        filename = argv[1];
-    }
+	if (argc < 2) {
+	    printf ("Usage : %s <filename>\n", argv[0]);
+	    exit (0);
+	} 
+	else {
+            printf("Loading %s ...\n", argv[1]);
+            filename = argv[1];
+        }
 
+	// Initialize the seed from the time
+	srand (time (NULL));
+	
 	// Analyze the file
-	Analyzer *analyzer = analyzer_new(filename);
+	Analyzer *analyzer = analyzer_new (filename);
 
 	// Get Desktop resolution and open the window
 	sfVideoMode desktop = sfVideoMode_getDesktopMode();
-	AppWindow *window = AppWindow_new("VisualBinary", desktop.width, desktop.height, false);
-	float *view = AppWindow_get_view(window);
+	AppWindow *window = AppWindow_new ("VisualBinary", desktop.width, desktop.height, false);
+	float *view = AppWindow_get_view (window);
 
-	/* Cube Visualisation */
-	Cube3D *cube = cube3d_new(SFML(window), analyzer);
-	cube3d_set_rot(cube, 0.0, 0.0);
+	// Cube Visualisation
+	Cube3D *cube = cube3d_new (SFML(window), analyzer);
+	cube3d_set_rot (cube, 0.0, 0.0);
 
 	// Cube Draw
 	DrawFunction *cube3d_draw_function = draw_function_new (cube3d_draw, cube, view);
-	int draw_state = AppWindow_add_draw_routine(window, cube3d_draw_function);
+	int draw_state = AppWindow_add_draw_routine (window, cube3d_draw_function);
 	AppWindow_set_state (window, draw_state);
 
 	// Cube Input
 	Function *cube3d_input_function = function_new (cube3d_input, cube);
-	AppWindow_add_input_routine(window, cube3d_input_function);
+	AppWindow_add_input_routine (window, cube3d_input_function);
 
 	// Cube Update
-	DrawFunction *cube3d_update_function = draw_function_new(cube3d_update, cube, view);
-	AppWindow_add_update_routine(window, cube3d_update_function);
+	DrawFunction *cube3d_update_function = draw_function_new (cube3d_update, cube, view);
+	AppWindow_add_update_routine (window, cube3d_update_function);
 
-	AppWindow_main(window);
+	AppWindow_main (window);
 
     return 0;
 }
